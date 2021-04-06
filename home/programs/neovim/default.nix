ea0fd242b525787cd606372a0fa458eda76837c4
Brian Romanko <hello@bromanko.com>
2021-04-06 08:48:22 -0500

    Switch Monokai to use vim-plug

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -15,12 +15,7 @@
     vim-highlightedyank
     vim-polyglot
     fzf-vim
-    {
-      plugin = lightline-vim;
-      config = ''
-        let g:lightline = {'colorscheme' : 'sonokai'}
-      '';
-    }
+    lightline-vim
     {
       plugin = vim-gitgutter;
       config = ''
@@ -32,21 +27,16 @@
         let g:gitgutter_sign_modified_removed   = '│'
       '';
     }
-    {
-      plugin = sonokai;
-      config = ''
-        if has('termguicolors')
-          set termguicolors
-        endif
-        let g:sonokai_style = 'shusia'
-        let g:sonokai_enable_italic = 1
-        let g:sonokai_better_performance = 1
-         " colorscheme sonokai
-      '';
-    }
   ];
 
   programs.neovim.extraConfig = ''
+    " plugins
+    call plug#begin(stdpath('data') . '/plugins')
+
+    Plug 'sainnhe/sonokai'
+
+    call plug#end()
+
     " keymaps
     let mapleader = " "
     let maplocalleader = ","
@@ -70,14 +60,35 @@
     " Replace grep with rg
     set grepprg=rg\ --vimgrep\ --smart-case\ --follow
 
-    " ui"
+    " ui
+    if has('termguicolors')
+      set termguicolors
+    endif
+
+    let g:sonokai_style = 'shusia'
+    let g:sonokai_enable_italic = 1
+    let g:sonokai_better_performance = 1
+    colorscheme sonokai
+    let g:lightline = {'colorscheme' : 'sonokai'}
+
     set colorcolumn=80,120  " highlight columns
-    set foldmethod=syntax   " enable syntax-aware folding
 
     " general
     au FocusLost * :wa      " save when losing focus
     set updatetime=100      " update UI elements more frequently
+
     " Don't use default settings for resize
     let g:vim_resize_disable_auto_mappings = 1
+  '';
+
+  xdg.dataFile."nvim/site/autoload/plug.vim".source = pkgs.fetchFromGitHub {
+    owner = "junegunn";
+    repo = "vim-plug";
+    rev = "68fef9c2fd9d4a21b500cc2249b6711a71c6fb9f";
+    sha256 = "0azmnxq82frs375k5b9yjdvsjfmzjv92ifqnmniar19d96yh6swa";
+  } + "/plug.vim";
+
+  home.activation.neovim = lib.hm.dag.entryAfter [ "installPackages" ] ''
+    vim +'packal' +'PlugInstall --sync' +qa
   '';
 }
