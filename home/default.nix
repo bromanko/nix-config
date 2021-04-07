9e753cfb0c3d17463b99319aed6ff2fe7cf9713c
Brian Romanko <hello@bromanko.com>
2021-04-07 17:16:41 -0500

    Properly copy home-manager apps on macOS

--- home/default.nix
+++ home/default.nix
@@ -53,6 +53,33 @@
       zsh-fast-syntax-highlighting
     ] ++ lib.optionals stdenv.isDarwin [ m-cli ];
 
+  home.activation = lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
+    copyApplications =
+      let
+        apps = pkgs.buildEnv {
+          name = "home-manager-applications";
+          paths = config.home.packages;
+          pathsToLink = "/Applications";
+        };
+      in
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        # Install MacOS applications to the user environment.
+        HM_APPS="$HOME/Applications/Home Manager Apps"
+
+        # Reset current state
+        [ -e "$HM_APPS" ] && $DRY_RUN_CMD rm -r "$HM_APPS"
+        $DRY_RUN_CMD mkdir -p "$HM_APPS"
+
+        # .app dirs need to be actual directories for Finder to detect them as Apps.
+        # The files inside them can be symlinks though.
+        $DRY_RUN_CMD cp --recursive --symbolic-link --no-preserve=mode -H ${apps}/Applications/* "$HM_APPS"
+        # Modes need to be stripped because otherwise the dirs wouldn't have +w,
+        # preventing us from deleting them again
+        # In the env of Apps we build, the .apps are symlinks. We pass all of them as
+        # arguments to cp and make it dereference those using -H
+      '';
+  };
+
   # This value determines the Home Manager release that your configuration is compatible with. This
   # helps avoid breakage when a new Home Manager release introduces backwards incompatible changes.
   #
