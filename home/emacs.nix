99763cafafa7f0c2e1831882bdb17b824689597c
Brian Romanko <hello@bromanko.com>
2021-06-03 04:50:02 -0500

    Attempt to add needed emacs dependencies

--- home/emacs.nix
+++ home/emacs.nix
@@ -6,5 +6,13 @@
       enable = true;
       doomPrivateDir = ./programs/emacs/doom.d;
       emacsPackage = if pkgs.stdenv.isDarwin then pkgs.emacsMacport else pkgs.emacs;
+      emacsPackagesOverlay = self: super: {
+        lsp-mode = super.lsp-mode.overrideattrs (esuper: {
+          buildinputs = esuper.buildinputs ++ [ pkgs.elixir_ls ];
+        });
+        magit-delta = super.magit-delta.overrideattrs (esuper: {
+          buildinputs = esuper.buildinputs ++ [ pkgs.git ];
+        });
+      };
     };
 }
