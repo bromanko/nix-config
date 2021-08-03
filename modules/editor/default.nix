325e0cfa057f92befff089d848f7b5ecfc7fe3a0
Brian Romanko <hello@bromanko.com>
2021-08-03 16:22:59 -0700

    Fix zsh config

--- modules/editor/default.nix
+++ modules/editor/default.nix
@@ -6,5 +6,8 @@ let cfg = config.modules.editors;
 in {
   options.modules.editors = { default = mkOpt types.str "vim"; };
 
-  config = mkIf (cfg.default != null) { env.EDITOR = cfg.default; };
+  config = mkIf (cfg.default != null) {
+    home-manager.users."${config.user.name}".home.sessionVariables.EDITOR =
+      cfg.default;
+  };
 }
--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -44,53 +44,56 @@ let
   };
 
   commonCfg = {
-    home-manager.users."${config.user.name}".home.sessionVariables = {
-      # zsh-vim-mode
-      MODE_CURSOR_VIINS = "#ffffff steady bar";
-      MODE_CURSOR_VICMD = "#ffffff steady block";
-    };
+    home-manager.users."${config.user.name}" = {
+      home.sessionVariables = {
+        SHELL = "${pkgs.zsh}/bin/zsh";
 
-    programs.zsh = {
-      enable = true;
+        # zsh-vim-mode
+        MODE_CURSOR_VIINS = "#ffffff steady bar";
+        MODE_CURSOR_VICMD = "#ffffff steady block";
+      };
 
-      enableAutosuggestions = true;
-      enableCompletion = true;
-      history.extended = true;
+      programs.zsh = {
+        enable = true;
 
-      shellAliases = shellAliases;
+        enableAutosuggestions = true;
+        enableCompletion = true;
+        history.extended = true;
 
-      plugins = [
-        {
-          name = "zsh-bromanko-functions";
-          src = ../../configs/zsh/plugins/zsh-bromanko-functions;
-        }
-        {
-          name = "zsh-vim-mode";
-          src = pkgs.fetchFromGitHub {
-            owner = "softmoth";
-            repo = "zsh-vim-mode";
-            rev = "1f9953b";
-            sha256 = "a+6EWMRY1c1HQpNtJf5InCzU7/RphZjimLdXIXbO6cQ=";
-          };
-        }
-        {
-          name = "zsh-fast-syntax-highlighting";
-          file = "fast-syntax-highlighting.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "zdharma";
-            repo = "fast-syntax-highlighting";
-            rev = "v1.55";
-            sha256 = "019hda2pj8lf7px4h1z07b9l6icxx4b2a072jw36lz9bh6jahp32";
-          };
-        }
-      ];
+        shellAliases = shellAliases;
+
+        plugins = [
+          {
+            name = "zsh-bromanko-functions";
+            src = ../../configs/zsh/plugins/zsh-bromanko-functions;
+          }
+          {
+            name = "zsh-vim-mode";
+            src = pkgs.fetchFromGitHub {
+              owner = "softmoth";
+              repo = "zsh-vim-mode";
+              rev = "1f9953b";
+              sha256 = "a+6EWMRY1c1HQpNtJf5InCzU7/RphZjimLdXIXbO6cQ=";
+            };
+          }
+          {
+            name = "zsh-fast-syntax-highlighting";
+            file = "fast-syntax-highlighting.plugin.zsh";
+            src = pkgs.fetchFromGitHub {
+              owner = "zdharma";
+              repo = "fast-syntax-highlighting";
+              rev = "v1.55";
+              sha256 = "019hda2pj8lf7px4h1z07b9l6icxx4b2a072jw36lz9bh6jahp32";
+            };
+          }
+        ];
+      };
     };
   };
 
   darwinCfg = {
     environment.shells = [ pkgs.zsh ];
     environment.loginShell = pkgs.zsh;
-    environment.variables.SHELL = "${pkgs.zsh}/bin/zsh";
 
     # Completion for system packages
     environment.pathsToLink = [ "/share/zsh" ];
