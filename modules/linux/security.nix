d9f66e5cdc5de5a2b849ccebe4e5396f6e04217e
Brian Romanko <hello@bromanko.com>
2021-08-06 15:40:02 -0700

    Add security settings for linux

---
+++ modules/linux/security.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
+  # on ssd systems, and volatile! Because it's wiped on reboot.
+  boot.tmpOnTmpfs = lib.mkDefault true;
+  # If not using tmpfs, which is naturally purged on reboot, we must clean it
+  # /tmp ourselves. /tmp should be volatile storage!
+  boot.cleanTmpDir = lib.mkDefault (!config.boot.tmpOnTmpfs);
+
+  # Change me later!
+  user.initialPassword = "nixos";
+  users.users.root.initialPassword = "nixos";
+}
