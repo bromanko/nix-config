211a9362d7afaa7c8ec8cb58f6d4ae821324ee22
Brian Romanko <hello@bromanko.com>
2021-08-30 08:14:44 -0700

    Add aliases for pbcopy and pbpaste on linux

--- modules/linux/shell/zsh.nix
+++ modules/linux/shell/zsh.nix
@@ -3,6 +3,15 @@
 with lib;
 with lib.my; {
   config = {
+    home-manager.users."${config.user.name}" = {
+      programs.zsh = {
+        shellAliases = {
+          pbcopy = "xclip -selection clipboard";
+          pbpaste = "xclip -selection clipboard -o";
+        };
+      };
+    };
+
     # Completion for system packages
     environment.pathsToLink = [ "/share/zsh" ];
   };
