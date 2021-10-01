5fdd8d9a9cedb19e0ff0a57ecb14f9e376f75209
Brian Romanko <hello@bromanko.com>
2021-10-01 11:02:21 -0700

    Move the common platform files

--- lib/hosts.nix
+++ lib/hosts.nix
@@ -17,7 +17,7 @@ with inputs; {
             mkDefault (removeSuffix ".nix" (baseNameOf path));
         }
         # This code is same on all architectures
-        ../hosts/x86_64-darwin/default.nix
+        ../hosts/darwin.nix
         (import path)
       ];
     };
@@ -32,13 +32,36 @@ with inputs; {
           networking.hostName =
             mkDefault (removeSuffix ".nix" (baseNameOf path));
         }
-        ../hosts/nixos/default.nix
+        ../hosts/nixos.nix
         (import path)
       ];
+    };
+
+  mkHmHost = system: path:
+    home-manager.lib.homeManagerConfiguration {
+      inherit system;
+      pkgs = pkgs.${system};
+      extraSpecialArgs = { inherit lib inputs; };
+      extraModules = [
+        ../hosts/home-manager.nix
+        # (import path)
+      ];
+      homeDirectory = "/home/bromanko";
+      username = "bromanko";
+      configuration = {
+        nixpkgs = {
+          config = pkgs.${system}.config;
+          overlays = pkgs.${system}.overlays;
+        };
+        # imports = [ ];
+      };
     };
 
   mapDarwinHosts = system: dir:
     mapModules dir (hostPath: mkDarwinHost system hostPath);
 
   mapNixosHosts = dir: mapModules dir (hostPath: mkNixosHost hostPath);
+
+  mapHomeManagerHosts = system: dir:
+    mapModules dir (hostPath: mkHmHost system hostPath);
 }
