30931cef83d040d0e56e8d3712e1e4920e02f167
Brian Romanko <hello@bromanko.com>
2021-12-09 17:01:14 -0800

    Change install script for nix 2.4

--- install-nix.sh
+++ install-nix.sh
@@ -7,20 +7,20 @@ URL="https://nixos.org/nix/install"
 
 # Nix
 if command -v nix >/dev/null; then
-	echo "nix is already installed on this system."
+    echo "nix is already installed on this system."
 else
-	[[ $(uname -s) = "Darwin" ]] && FLAG="--darwin-use-unencrypted-nix-store-volume"
-	sh <(curl -L $URL) $FLAG
+    [[ $(uname -s) = "Darwin" ]] && FLAG="--darwin-use-unencrypted-nix-store-volume"
+    sh <(curl -L "$URL") $FLAG
 fi
 
 # Flakes
 if nix flake >/dev/null 2>&1; then
-   echo "nix Flakes is already installed on this system."
+    echo "nix Flakes is already installed on this system."
 else
-    nix-env -iA nixpkgs.nixFlakes
-    if ! grep "experimental-features" < ~/.config/nix/nix.conf >/dev/null; then
+    nix-env -iA nixpkgs.nix
+    if ! grep "experimental-features" <~/.config/nix/nix.conf >/dev/null; then
         mkdir -p ~/.config/nix
         touch ~/.config/nix/nix.conf
-        echo "experimental-features = nix-command flakes" >> ~/.config/nix/nix.conf
+        echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf
     fi
 fi
