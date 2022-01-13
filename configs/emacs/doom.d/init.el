2c1e6a52b151045dff0234ca39c385c9266b9d62
Brian Romanko <hello@bromanko.com>
2022-01-13 15:07:42 -0800

    emacs: Switch to vertico

--- configs/emacs/doom.d/init.el
+++ configs/emacs/doom.d/init.el
@@ -19,10 +19,10 @@
        :completion
        (company
          +childframe)      ; the ultimate code completion backend
-       (ivy                ; a search engine for love and life
-        +fuzzy
-        +icons
-        +prescient)
+       ;;helm              ; the *other* search engine for love and life
+       ;;ido               ; the other *other* search engine...
+       ;;ivy               ; a search engine for love and life
+       vertico             ; the search engine of the future
 
        :ui
        deft                ; notational velocity for Emacs
