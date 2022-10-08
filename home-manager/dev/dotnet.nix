5813ab3a07f17abdff3ea32ddeeac532e82af08c
Brian Romanko <hello@bromanko.com>
2022-10-07 20:22:17 -0700

    Upgrade to dotnet 7

--- home-manager/dev/dotnet.nix
+++ home-manager/dev/dotnet.nix
@@ -4,6 +4,7 @@ with lib;
 with lib.my;
 let cfg = config.modules.dev.dotnet;
 in {
-  config =
-    mkIf cfg.enable { home = { packages = with pkgs; [ dotnet-sdk mono ]; }; };
+  config = mkIf cfg.enable {
+    home = { packages = with pkgs; [ dotnetCorePackages.sdk_7_0 mono ]; };
+  };
 }
