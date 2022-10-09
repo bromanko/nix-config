ffa3e633f91ee7117006974b6a4259ab70683e26
Brian Romanko <hello@bromanko.com>
2022-10-09 09:33:23 -0700

    Switch back to dotnet 6

--- home-manager/dev/dotnet.nix
+++ home-manager/dev/dotnet.nix
@@ -5,6 +5,6 @@ with lib.my;
 let cfg = config.modules.dev.dotnet;
 in {
   config = mkIf cfg.enable {
-    home = { packages = with pkgs; [ dotnetCorePackages.sdk_7_0 mono ]; };
+    home = { packages = with pkgs; [ dotnetCorePackages.sdk_6_0 mono ]; };
   };
 }
