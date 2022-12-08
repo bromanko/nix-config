535131ff0e5282f79f9058b52e5688a33fd49d96
Brian Romanko <hello@bromanko.com>
2022-12-07 20:23:08 -0800

    Module for configuring macOS dictionaries

---
+++ modules/darwin/desktop/dictionaries.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.dictionaries;
+in {
+  options.modules.desktop.dictionaries = with types; {
+    enable = mkBoolOpt false;
+
+    dictionaries = mkOption {
+      type = types.listOf types.path;
+      default = [ pkgs.my.websters-1913 ];
+      example = literalExpression "[ pkgs.my.websters-1913 ]";
+      description = "List of dictionaries to install.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    system.activationScripts.postActivation.text = ''
+      # Set up dictionaries.
+      echo "configuring dictionaries..." >&2
+      mkdir -p ~/Library/Dictionaries
+
+      declare -a dictionaries=( ${toString (lib.toList cfg.dictionaries)} )
+      for path in $dictionaries; do
+        find -L $path -type d -name "*.dictionary" -print0 | while IFS= read -rd "" f; do
+          # Must copy the dictionary since symlinks don't work with Dictionary.app
+          cp -rf "$f" ~/Library/Dictionaries
+        done
+      done
+    '';
+  };
+}
