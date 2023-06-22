e1e9538d2a5bea9cb2aed70d9a3ae0a6c6bb6072
Brian Romanko <bromanko@meta.com>
2023-06-22 15:47:37 -0700

    Add home-manager config for nixos machines

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -22,5 +22,18 @@ with lib.my; {
 
   users.users.${config.user.name} = mkAliasDefinitions config.user;
 
+  hm = {
+    home = {
+      # This value determines the Home Manager release that your configuration
+      # is compatible with. This helps avoid breakage when a new Home Manager
+      # release introduces backwards incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See the Home
+      # Manager release notes for a list of state version changes in each
+      # release.
+      stateVersion = "21.03";
+    };
+  };
+
   environment.systemPackages = with pkgs; [ xorg.xdpyinfo killall git ];
 }
