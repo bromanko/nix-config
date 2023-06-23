8a35eac26297f13d38d45b84fbea752f78c3939e
Brian Romanko <bromanko@meta.com>
2023-06-22 17:03:59 -0700

    Add public key for work machine

--- modules/linux/users.nix
+++ modules/linux/users.nix
@@ -6,5 +6,10 @@
     extraGroups = [ "wheel" ];
     home = "/home/${config.user.name}";
     shell = pkgs.zsh;
+    openssh = {
+      authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2vkvKlul2zm/Qx7V0NmmwGDJcFY46tf9asOVONkcCK Meta MacBook Pro 16"
+      ];
+    };
   };
 }
