1b843480d746f15817b0f91eaf62919580c13ad1
Brian Romanko <bromanko@fb.com>
2023-09-25 14:28:33 -0700

    Switch darwin to use emacs-overlay

--- modules/darwin/editor/emacs.nix
+++ modules/darwin/editor/emacs.nix
@@ -1,22 +1,19 @@
-{ config, pkgs, lib, inputs, ... }:
+{ config, pkgs, lib, ... }:
 
 with lib;
 
 let cfg = config.modules.editor.emacs;
 in {
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [ inputs.emacs-overlay-darwin.overlay ];
-    nix.settings = {
-      substituters = [ "https://cachix.org/api/v1/cache/emacs" ];
-      trusted-public-keys =
-        [ "emacs.cachix.org-1:b1SMJNLY/mZF6GxQE+eDBeps7WnkT0Po55TAyzwOxTY=" ];
-    };
-
     home-manager.users."${config.user.name}" = {
-      programs.emacs = {
+      programs.emacs = with pkgs; {
         enable = true;
-        package = pkgs.emacs;
+        package = emacs-git;
+        extraPackages = epkgs: [ epkgs.vterm ];
       };
+      # needed to compile vterm
+      home.packages = with pkgs; [ libtool ];
     };
   };
 }
+
