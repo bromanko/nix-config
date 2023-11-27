88357eef28acae4cad6d05c670d1c543969a24b6
Brian Romanko <bromanko@fb.com>
2023-11-27 13:46:14 -0800

    Add exa shell aliases for fish

--- home-manager/shell/exa.nix
+++ home-manager/shell/exa.nix
@@ -2,15 +2,20 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.shell.exa;
+let
+  cfg = config.modules.shell.exa;
+  shellAliases = {
+    ls = "${pkgs.eza}/bin/eza";
+    ll = "ls -l --time-style long-iso --icons";
+    l = "ll -a";
+  };
 in {
   config = mkIf cfg.enable {
     home.packages = [ pkgs.eza ];
 
-    programs.zsh.shellAliases = mkIf config.modules.shell.zsh.enable {
-      ls = "${pkgs.eza}/bin/eza";
-      ll = "ls -l --time-style long-iso --icons";
-      l = "ll -a";
-    };
+    programs.zsh.shellAliases =
+      mkIf config.modules.shell.zsh.enable shellAliases;
+    programs.fish.shellAliases =
+      mkIf config.modules.shell.fish.enable shellAliases;
   };
 }
