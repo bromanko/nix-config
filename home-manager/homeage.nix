4e34aee322d59836eb79e94394fb69e0454ac740
Brian Romanko <bromanko@fb.com>
2024-02-22 13:28:01 -0800

    Add initial homeage module

--- home-manager/homeage.nix
+++ home-manager/homeage.nix
@@ -1,8 +1,12 @@
-{ config, lib, inputs, ... }:
+{ config, lib, inputs, pkgs, ... }:
 
 with lib;
 let cfg = config.modules.homeage;
 in {
   imports = [ inputs.homeage.homeManagerModules.homeage ];
-  config = mkIf cfg.enable { };
+  config = mkIf cfg.enable {
+    homeage = { inherit (cfg) installationType; };
+
+    home = { packages = with pkgs; [ my.age-with-plugins my.age-plugin-op ]; };
+  };
 }
--- modules/home-manager/homeage.nix
+++ modules/home-manager/homeage.nix
@@ -2,5 +2,46 @@
 
 with lib;
 with lib.my; {
-  options.modules.homeage = { enable = mkBoolOpt false; };
+  options.modules.homeage = {
+    enable = mkBoolOpt false;
+
+    installationType = mkOption {
+      type = types.enum [ "activation" "systemd" ];
+      default = "activation";
+      example = "activation";
+      description = ''
+        Specify the way how secrets should be installed. Either via systemd user services (<literal>systemd</literal>)
+        or during the activation of the generation (<literal>activation</literal>).
+        </para><para>
+        Note: Keep in mind that symlinked secrets will not work after reboots with <literal>activation</literal> if
+        <literal>homeage.mount</literal> does not point to persistent location.
+
+        Cleanup notes:
+        * Systemd performs cleanup when service stops.
+        * Activation performs cleanup after write boundary during activation.
+        * When switching from systemd to activation, may need to activate twice.
+          Because stopping systemd services, and thus cleanup, happens after
+          activation decryption. Only occurs on the first activation.
+
+        Cases when copied file/symlink is not removed:
+        1. Symlink does not point to the decrypted secret file.
+        2. Any copied file when the original secret file does not exist (can't verify they weren't modified).
+        3. Copied file when it does not match the original secret file (using `cmp`).
+      '';
+    };
+
+    pkg = mkOption {
+      description =
+        "(R)age package to use. Detects if using rage and switches to `rage` as the command rather than `age`";
+      default = pkgs.age;
+      type = types.package;
+    };
+
+    identityPaths = mkOption {
+      description =
+        "Absolute path to identity files used for age decryption. Must provide at least one path";
+      default = [ ];
+      type = types.listOf types.str;
+    };
+  };
 }
