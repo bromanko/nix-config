25d50afb4256a1605611f380f96c0940397d8648
Brian Romanko <hello@bromanko.com>
2024-02-28 09:17:11 -0800

    Add jujutsu settings

--- modules/home-manager/shell/jujutsu.nix
+++ modules/home-manager/shell/jujutsu.nix
@@ -4,11 +4,31 @@ with lib;
 with lib.my;
 let cfg = config.modules.shell.jujutsu;
 in {
-  options.modules.shell.jujutsu = { enable = mkBoolOpt false; };
+  options.modules.shell.jujutsu = {
+    enable = mkBoolOpt false;
+
+    userEmail = mkOption {
+      type = types.str;
+      default = "hello@bromanko.com";
+    };
+
+    userName = mkOption {
+      type = types.str;
+      default = "Brian Romanko";
+    };
+  };
 
   config = mkIf cfg.enable {
     hm = {
-      programs.jujutsu = { enable = true; };
+      programs.jujutsu = {
+        enable = true;
+        settings = {
+          user = {
+            name = cfg.userName;
+            email = cfg.userEmail;
+          };
+        };
+      };
 
       programs.git = { ignores = [ ".jj" ]; };
     };
