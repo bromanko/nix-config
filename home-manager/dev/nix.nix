4fede5220282e85f647a378381de0f5025715d4b
Brian Romanko <hello@bromanko.com>
2024-03-22 14:59:48 -0700

    Add nil to nix dependencies

--- home-manager/dev/nix.nix
+++ home-manager/dev/nix.nix
@@ -5,6 +5,6 @@ with lib.my;
 let cfg = config.modules.dev.nix;
 in {
   config = mkIf cfg.enable {
-    home = { packages = with pkgs; [ nixfmt nix-output-monitor ]; };
+    home = { packages = with pkgs; [ nixfmt nix-output-monitor nil ]; };
   };
 }
