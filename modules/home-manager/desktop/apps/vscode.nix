f626ec983ab04dc0292025a0fb91ed8953f31dd7
Brian Romanko <hello@bromanko.com>
2024-03-22 13:04:31 -0700

    Add vscode extensions

--- modules/home-manager/desktop/apps/vscode.nix
+++ modules/home-manager/desktop/apps/vscode.nix
@@ -6,5 +6,27 @@ let cfg = config.modules.desktop.apps.vscode;
 in {
   options.modules.desktop.apps.vscode = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { hm = { programs.vscode = { enable = true; }; }; };
+  config = mkIf cfg.enable {
+    hm = {
+      programs.vscode = {
+        enable = true;
+        extensions = with pkgs.vscode-extensions; [
+          catppuccin.catppuccin-vsc
+          catppuccin.catppuccin-vsc-icons
+          serayuzgur.crates
+          mkhl.direnv
+          kahole.magit
+          tamasfe.even-better-toml
+          bodil.file-browser
+          github.copilot
+          github.copilot-chat
+          bbenoist.nix
+          rust-lang.rust-analyzer
+          vscodevim.vim
+          vspacecode.vspacecode
+          vspacecode.whichkey
+        ];
+      };
+    };
+  };
 }
