fc1f77f8ab92130e92206eed44c4160e07fc073d
Brian Romanko <hello@bromanko.com>
2024-05-30 16:04:48 -0700

    Add settings and keymap for zed

--- modules/home-manager/editor/zed.nix
+++ modules/home-manager/editor/zed.nix
@@ -6,5 +6,39 @@ let cfg = config.modules.editor.zed;
 in {
   options.modules.editor.zed = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { homebrew = { casks = [ "zed@preview" ]; }; };
+  config = mkIf cfg.enable {
+    homebrew = { casks = [ "zed@preview" ]; };
+    hm = {
+      home = {
+        activation.zedConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          if [ ! -f "$HOME/.config/zed/settings.json" ]; then
+            echo "Writing Zed settings.json"
+            cp ${
+              ../../../configs/zed/settings.json
+            } "$HOME/.config/zed/settings.json"
+          else
+            if ! cmp ${
+              ../../../configs/zed/settings.json
+            } "$HOME/.config/zed/settings.json"; then
+              echo "Zed settings.json exists and is different"
+              exit 1
+            fi
+          fi
+          if [ ! -f "$HOME/.config/zed/keymap.json" ]; then
+            echo "Writing Zed keymap.json"
+            cp ${
+              ../../../configs/zed/keymap.json
+            } "$HOME/.config/zed/keymap.json"
+          else
+            if ! cmp ${
+              ../../../configs/zed/keymap.json
+            } "$HOME/.config/zed/keymap.json"; then
+              echo "Zed keymap.json exists and is different"
+              exit 1
+            fi
+          fi
+        '';
+      };
+    };
+  };
 }
