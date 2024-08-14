21886e4bde7d431e455adf70d4a820db6d97f23a
Brian Romanko <bromanko@meta.com>
2024-08-14 15:47:41 -0700

    Configure wezterm

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,3 +1,9 @@
+local function prequire(m)
+  local ok, err = pcall(require, m)
+  if not ok then return nil, err end
+  return err
+end
+
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
@@ -5,6 +11,22 @@ local config = wezterm.config_builder()
 config.font = wezterm.font('MonaspiceAr Nerd Font Mono', { weight = 'Light' })
 config.font_size = 13
 
+-- appearance
 config.color_scheme = 'Catppuccin Mocha'
+config.window_decorations = 'RESIZE'
+config.window_frame = {
+    font = wezterm.font({ family = 'SF UI Display'}),
+    font_size = 15,
+}
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_scroll_bar = true
+
+
+-- Load any configuration not managed by home-manager
+-- This is useful for iterating on changes without having to rebuild the nix config
+local overrides = prequire('overrides')
+if overrides and overrides.apply_config then
+  overrides.apply_config(config)
+end
 
 return config
