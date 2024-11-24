ac3ce8a5d6d255f7ddff3d947bcf0a3464cbfbb6
Brian Romanko <hello@bromanko.com>
2024-11-24 15:47:36 -0800

    Move the nixos LiveCD to an ISO folder
    Fix errors in iso.nix for mac mini livecd

---
+++ hosts/iso/macmini/default.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  boot.kernelModules = [ "wl" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.blacklistedKernelModules = [ "b43" "bcma" ];
+  networking.wireless.enable = false;
+
+  # Need to use iwd due to a bug with Broadcom adapters
+  # in version 2.10 of wpa_supplicant.
+  networking.wireless.iwd.enable = true;
+}
