8f3439dd6f1000ede7ae0ca99834c8c25b232973
Brian Romanko <hello@bromanko.com>
2024-12-22 11:58:54 -0800

    Split zed config and option

---
+++ home-manager/editor/zed.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+with lib;
+let
+  cfg = config.modules.editor.zed;
+in
+{
+  config = mkIf cfg.enable {
+    homebrew = {
+      casks = [ "zed@preview" ];
+    };
+    hm = {
+      home = {
+        packages = with pkgs; [ nixd ];
+        activation.zedConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          if [ ! -f "$HOME/.config/zed/settings.json" ]; then
+            echo "Writing Zed settings.json"
+            cp ${../../../configs/zed/settings.json} "$HOME/.config/zed/settings.json"
+          else
+            if ! cmp ${../../../configs/zed/settings.json} "$HOME/.config/zed/settings.json"; then
+              echo "Zed settings.json exists and is different"
+              exit 1
+            fi
+          fi
+          if [ ! -f "$HOME/.config/zed/keymap.json" ]; then
+            echo "Writing Zed keymap.json"
+            cp ${../../../configs/zed/keymap.json} "$HOME/.config/zed/keymap.json"
+          else
+            if ! cmp ${../../../configs/zed/keymap.json} "$HOME/.config/zed/keymap.json"; then
+              echo "Zed keymap.json exists and is different"
+              exit 1
+            fi
+          fi
+        '';
+      };
+    };
+  };
+}
