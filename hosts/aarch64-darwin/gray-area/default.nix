93b1b98ba6d5365cf3a267228af48a83cd4bff49
Brian Romanko <hello@bromanko.com>
2024-12-29 18:31:48 -0800

    Initial config for gray-area Add config for new mac mini

---
+++ hosts/aarch64-darwin/gray-area/default.nix
@@ -0,0 +1,87 @@
+{ pkgs, lib, ... }:
+
+let
+  brewPath = "/opt/homebrew/bin";
+in
+with lib;
+with lib.my;
+{
+  modules = {
+    homeage = {
+      enable = true;
+      file = {
+        "nix.config" = {
+          source = ../../../configs/nix/nix.conf.age;
+          symlinks = [ "$HOME/.config/nix/nix.conf" ];
+        };
+      };
+    };
+    shell = {
+      commonPkgs.enable = true;
+      ssh.enable = true;
+      openssh.enable = true;
+      fish = {
+        enable = true;
+        extraPaths = [
+          "$HOME/bin"
+          brewPath
+        ];
+      };
+      bat.enable = true;
+      git.enable = true;
+      jujutsu.enable = true;
+      starship.enable = true;
+      fzf.enable = true;
+      direnv.enable = true;
+      exa.enable = true;
+      fd.enable = true;
+    };
+    desktop = {
+      fonts.enable = true;
+      apps = {
+        raycast.enable = true;
+        "1Password".enable = true;
+      };
+    };
+    dev = {
+      docker.enable = true;
+      nix.enable = true;
+    };
+    term = {
+      ghostty.enable = true;
+    };
+    editor = {
+      default = "nvim";
+      visual = "zed-preview -w";
+      neovim.enable = true;
+      zed.enable = true;
+    };
+
+    homebrew = {
+      enable = true;
+      brewPrefix = brewPath;
+      taps = [
+        "homebrew/cask-versions"
+        "homebrew/services"
+      ];
+      casks = [
+        "betterdisplay"
+        "ghostty"
+        "istat-menus"
+        "jordanbaird-ice"
+        "arc"
+      ];
+      masApps = {
+        Xcode = 497799835;
+      };
+    };
+  };
+  hm = {
+    home = {
+      packages = with pkgs; [
+        tailscale
+      ];
+    };
+  };
+  services.nix-daemon.enable = true;
+}
