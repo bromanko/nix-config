1ee6e194b690d8ce9c0a0ebe6eb348689cfa82dc
Brian Romanko <hello@bromanko.com>
2024-12-30 20:42:04 -0800

    Remove home-manager configs

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,21 +1,24 @@
-{ config, lib, pkgs, options, inputs, ... }:
+{
+  config,
+  lib,
+  options,
+  ...
+}:
 
 with lib;
-with lib.my; {
+with lib.my;
+{
   options = {
     hm = mkOpt' types.attrs { } "Passthrough for home-manager configuration";
   };
   config = {
-    # Bootstrap the home-manager config
-    hm = import ../home-manager { inherit config lib pkgs options inputs; };
-
     home-manager = {
       useGlobalPkgs = true;
       backupFileExtension = "orig";
 
       # Workaround to enable installing via `nixos-install`
       # https://github.com/nix-community/home-manager/issues/1262
-      sharedModules = [{ manual.manpages.enable = false; }];
+      sharedModules = [ { manual.manpages.enable = false; } ];
 
       # map the hm config to default home-manager user
       users."${config.user.name}" = mkAliasDefinitions options.hm;
