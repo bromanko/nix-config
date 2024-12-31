ab6000d465293d270d725b6735d3ba47f1263721
Brian Romanko <hello@bromanko.com>
2024-12-30 21:17:42 -0800

    Add catppuccin theme to bat

--- modules/home-manager/shell/bat.nix
+++ modules/home-manager/shell/bat.nix
@@ -24,7 +24,18 @@ in
       programs.bat = {
         enable = true;
         config = {
-          theme = "Monokai Extended";
+          theme = "Catppuccin Mocha";
+        };
+        themes = {
+          catppuccinMocha = {
+            src = pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "bat"; # Bat uses sublime syntax for its themes
+              rev = "699f60fc8ec434574ca7451b444b880430319941";
+              sha256 = "sha256-6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
+            };
+            file = "themes/Catppuccin Mocha.tmTheme";
+          };
         };
       };
 
