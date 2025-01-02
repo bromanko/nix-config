773db474640055630901c8152322bfd84a73eea9
Brian Romanko <hello@bromanko.com>
2025-01-02 10:48:27 -0800

    Add helper to create a symlink to the nix-config repo Add helper for mkOutofStoreSymlink

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,7 +1,7 @@
 {
+  inputs,
   config,
   lib,
-  options,
   ...
 }:
 
@@ -12,6 +12,13 @@ with lib.my;
     (mkAliasOptionModule [ "hm" ] [ "home-manager" "users" config.user.name ])
   ];
   options = {
+    nixConfigPath = lib.mkOption {
+      type = lib.types.path;
+      apply = toString;
+      default = "${config.users.users.${config.user.name}.home}/Code/nix-config";
+      example = "${config.users.users.${config.user.name}.home}/Code/nix-config";
+      description = "Location of the nix-config working copy";
+    };
   };
   config = {
     home-manager = {
@@ -21,7 +28,18 @@ with lib.my;
       # Workaround to enable installing via `nixos-install`
       # https://github.com/nix-community/home-manager/issues/1262
       sharedModules = [ { manual.manpages.enable = false; } ];
+    };
 
+    hm = {
+      lib = {
+        file = {
+          mkNixConfigSymlink =
+            path:
+            config.hm.lib.file.mkOutOfStoreSymlink (
+              config.nixConfigPath + removePrefix (toString inputs.self) (toString path)
+            );
+        };
+      };
     };
   };
 }
