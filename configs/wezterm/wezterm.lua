9addf0fb6d77eaeffc46e721a6105243b35699c3
Brian Romanko <bromanko@meta.com>
2025-01-04 11:50:41 -0800

    Fix wezterm on latest nix

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -7,4 +7,12 @@ config.font_size = 13
 
 config.color_scheme = 'Catppuccin Mocha'
 
+config.window_close_confirmation = 'NeverPrompt'
+
+-- force renderer due to bug in latest version of Wezterm
+-- see: https://github.com/wez/wezterm/issues/5990
+config.front_end = "WebGpu"
+config.webgpu_power_preference = 'HighPerformance'
+
+
 return config
