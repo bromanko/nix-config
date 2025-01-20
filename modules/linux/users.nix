c821110197fd3c6d0c32bb8c2271fc94c7551fe4
Brian Romanko <hello@bromanko.com>
2025-01-19 20:02:18 -0800

    Refactor authorized keys

--- modules/linux/users.nix
+++ modules/linux/users.nix
@@ -1,4 +1,8 @@
-{ config, options, lib, pkgs, ... }:
+{
+  config,
+  pkgs,
+  ...
+}:
 
 {
   users.users.${config.user.name} = {
@@ -7,10 +11,7 @@
     home = "/home/${config.user.name}";
     shell = pkgs.fish;
     openssh = {
-      authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2vkvKlul2zm/Qx7V0NmmwGDJcFY46tf9asOVONkcCK Meta MacBook Pro 16"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPzLxgUGkWXC/Hkvuxv4rsJfFYrYq1S16DouIXRXD2Ia Personal MacBook Air"
-      ];
+      authorizedKeys.keys = config.authorizedKeys;
     };
   };
 }
