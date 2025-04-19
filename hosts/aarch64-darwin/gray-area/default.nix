f0cc69272dd555b5b1bb9c4874b0068628bdcb65
Brian Romanko <hello@bromanko.com>
2025-04-19 04:51:41 -0700

    Disable nix config on gray-area

--- hosts/aarch64-darwin/gray-area/default.nix
+++ hosts/aarch64-darwin/gray-area/default.nix
@@ -1,9 +1,10 @@
-{ pkgs, lib, ... }:
+{ ... }:
 
 let
   brewPath = "/opt/homebrew/bin";
 in
 {
+  nix.enable = false;
   authorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2vkvKlul2zm/Qx7V0NmmwGDJcFY46tf9asOVONkcCK 1Password"
   ];
@@ -69,5 +70,4 @@ in
       };
     };
   };
-  services.nix-daemon.enable = true;
 }
