4210009aaef3be836ad087544a2f945bbe857751
Brian Romanko <hello@bromanko.com>
2025-05-04 11:06:28 -0700

    Add script to update Claude Code version
    This is forked from the script in the nixpkgs repo

---
+++ packages/claude-code/update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nodePackages.npm nix-update
+
+set -euo pipefail
+set -x
+
+version=$(npm view @anthropic-ai/claude-code version)
+
+# Generate updated lock file
+cd "$(dirname "${BASH_SOURCE[0]}")"
+npm i --package-lock-only @anthropic-ai/claude-code@"$version"
+rm -f package.json
+
+# Update version and hashes
+echo "$version"
