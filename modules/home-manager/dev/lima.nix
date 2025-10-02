7440b612c1fb2de6e0fc459408a887355d10d945
Brian Romanko <hello@bromanko.com>
2025-10-02 16:24:18 -0700

    Add Lima VM development environment module
    - Create Lima module that installs lima CLI and deploys VM config
    - Add dev.yaml config for Ubuntu 24.04 VM with:
      - VZ virtualization for macOS performance
      - 4 CPUs, 8GB RAM, 100GB disk
      - Nix installed via Determinate Systems installer
      - SSH key forwarding enabled
      - No filesystem mounts for isolation
    - Enable Lima module on arbitrary host

---
+++ modules/home-manager/dev/lima.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+with lib.my;
+let
+  cfg = config.modules.dev.lima;
+in
+{
+  options.modules.dev.lima = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    hm = {
+      home.packages = with pkgs; [ lima ];
+
+      home.file.".lima/_config/lima-dev.yaml" = {
+        source = ../../../configs/lima/dev.yaml;
+      };
+    };
+  };
+}
