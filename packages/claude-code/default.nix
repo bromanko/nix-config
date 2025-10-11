6eb9d6693aa95abdaa310f9b76b98f961fc6c7f5
Brian Romanko <hello@bromanko.com>
2025-10-11 10:18:20 -0700

    Update claude code version

--- packages/claude-code/default.nix
+++ packages/claude-code/default.nix
@@ -6,14 +6,14 @@
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "2.0.1";
+  version = "2.0.14";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-LUbDPFa0lY74MBU4hvmYVntt6hVZy6UUZFN0iB4Eno8=";
+    hash = "sha256-U/wd00eva/qbSS4LpW1L7nmPW4dT9naffeMkHQ5xr5o=";
   };
 
-  npmDepsHash = "sha256-t5pWRR+uzovKWtXiR4JEOA5IgbbBSifCrMJ3NAZb/1k=";
+  npmDepsHash = "sha256-iG4DjQ7UeEaQrftXsPooG3ujQH96JKx836aW0BHTR2k=";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
