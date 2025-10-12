3a66045f74e8549fd71457ff4daaabd659472a15
Brian Romanko <hello@bromanko.com>
2025-10-12 07:46:44 -0700

    fix: update rift source hash and migrate to new darwin SDK pattern

--- packages/rift.nix
+++ packages/rift.nix
@@ -1,8 +1,9 @@
 {
   lib,
+  stdenv,
   rustPlatform,
   fetchFromGitHub,
-  darwin,
+  apple-sdk_11,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "rift";
@@ -12,14 +13,13 @@ rustPlatform.buildRustPackage rec {
     owner = "acsandmann";
     repo = "rift";
     rev = "c0b2846b3342bcaa92a293453e35a092fe490aa4";
-    hash = lib.fakeHash;
+    hash = "sha256-mzALi9iAQv08FHNQTzOx6JteMz9TmQrSbFhKCKtAJkE=";
   };
 
   cargoHash = lib.fakeHash;
 
-  buildInputs = [
-    darwin.apple_sdk.frameworks.AppKit
-    darwin.apple_sdk.frameworks.ApplicationServices
+  buildInputs = lib.optionals stdenv.isDarwin [
+    apple-sdk_11
   ];
 
   # Disable tests - may require GUI/accessibility
