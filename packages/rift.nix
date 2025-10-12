8563ca8bf803d185d39180d8a79afc7bff3882b8
Brian Romanko <hello@bromanko.com>
2025-10-12 07:56:13 -0700

    fix: enable unstable Rust features for rift build
    Rift requires unstable Rust features (let_chains, stmt_expr_attributes)
    which are not available on stable Rust. Enable RUSTC_BOOTSTRAP=1 to
    allow these features to compile on the stable toolchain.

--- packages/rift.nix
+++ packages/rift.nix
@@ -22,6 +22,9 @@ rustPlatform.buildRustPackage rec {
     apple-sdk_11
   ];
 
+  # Enable unstable Rust features (let_chains, stmt_expr_attributes)
+  RUSTC_BOOTSTRAP = 1;
+
   # Disable tests - may require GUI/accessibility
   doCheck = false;
 
