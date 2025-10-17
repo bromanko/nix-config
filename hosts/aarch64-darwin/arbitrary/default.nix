d29f7bffcbb17088765f9398921340c08923ca3a
Brian Romanko <hello@bromanko.com>
2025-10-17 13:18:30 -0700

    feat(arbitrary): enable multitouch gestures

--- hosts/aarch64-darwin/arbitrary/default.nix
+++ hosts/aarch64-darwin/arbitrary/default.nix
@@ -44,6 +44,7 @@ with lib.my;
         "1Password".enable = true;
         vscode.enable = true;
         claude.enable = true;
+        multitouch.enable = true;
         aerospace = {
           enable = true;
           windowRules = [
