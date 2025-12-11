8f7c4f8f3e2d919e67b3d3809ae4d53465ff8eda
Brian Romanko <hello@bromanko.com>
2025-12-11 10:59:53 -0800

    Fix gemini cli overlay

--- overlays/gemini-cli.nix
+++ overlays/gemini-cli.nix
@@ -1,14 +1,19 @@
 final: prev: {
   gemini-cli = prev.gemini-cli.overrideAttrs (oldAttrs: rec {
-    version = "0.18.4";
+    version = "0.20.0";
 
     src = prev.fetchFromGitHub {
       owner = "google-gemini";
       repo = "gemini-cli";
       rev = "v${version}";
-      hash = "sha256-TSHL3X+p74yFGTNFk9r4r+nnul2etgVdXxy8x9BjsRg=";
+      hash = "sha256-6+fT9/npYrngAPeAP7pA6DYNuCVWm1lKpSVP4Ux4ddw=";
     };
 
-    npmDepsHash = "sha256-JvzrbyiJHbKNRHoGll7eSH4dD6Hj5qnrh4F/upHPntI=";
+    npmDeps = prev.fetchNpmDeps {
+      inherit src;
+      hash = "sha256-wbr/9IitwQxBVFskCyGZfWy6FmIGZAVYLbF/sMJ2X+s=";
+    };
+
+    # npmDepsHash = "sha256-wbr/9IitwQxBVFskCyGZfWy6FmIGZAVYLbF/sMJ2X+s=";
   });
 }
