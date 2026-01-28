0a2684f50a5d5c1db8daed876b6282ba2d0019f9
Brian Romanko <hello@bromanko.com>
2026-01-28 08:37:48 -0800

    feat(fish): add rebuild! function for darwin
    Add portable rebuild! fish function that:
    - Uses hostname -s to detect current host
    - Rebuilds from local ~/Code/nix-config if present
    - Falls back to building from github:bromanko/nix-config if local dir missing

--- modules/darwin/shell/fish.nix
+++ modules/darwin/shell/fish.nix
@@ -10,6 +10,8 @@ with lib.my;
 
 let
   cfg = config.modules.shell.fish;
+  nixConfigDir = "$HOME/Code/nix-config";
+  nixConfigRepo = "github:bromanko/nix-config";
 in
 {
   config = mkIf cfg.enable {
@@ -20,6 +22,22 @@ in
     environment = {
       shells = [ pkgs.fish ];
       pathsToLink = [ "/share/fish" ];
+    };
+
+    hm.programs.fish.functions = {
+      "rebuild!" = ''
+        set -l host (hostname -s)
+        if test -d ${nixConfigDir}
+          echo "Rebuilding from local config..."
+          sudo darwin-rebuild switch --flake ${nixConfigDir}#$host
+        else
+          echo "Local config not found, building from remote..."
+          set -l tmpdir (mktemp -d)
+          nix build ${nixConfigRepo}#darwinConfigurations.$host.system -o $tmpdir/result
+          and sudo $tmpdir/result/sw/bin/darwin-rebuild switch --flake ${nixConfigRepo}#$host
+          rm -rf $tmpdir
+        end
+      '';
     };
   };
 }
