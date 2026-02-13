f9059eef0a9af8d2ced109a3970f0685ed6dfb3a
Brian Romanko <hello@bromanko.com>
2026-02-13 09:25:45 -0700

    fix(shell): use real cat for binary files in fish

--- modules/home-manager/shell/bat.nix
+++ modules/home-manager/shell/bat.nix
@@ -40,7 +40,27 @@ in
       };
 
       programs.zsh.shellAliases = mkIf config.modules.shell.zsh.enable shellAliases;
-      programs.fish.shellAliases = mkIf config.modules.shell.fish.enable shellAliases;
+
+      # For fish, use a function that falls back to real cat for binary files
+      programs.fish = mkIf config.modules.shell.fish.enable {
+        functions.cat = ''
+          # Check if file exists and is a regular file
+          if test -f $argv[1]
+            # Use file command to detect binary files
+            if file -b --mime $argv[1] 2>/dev/null | grep -q '^text/'
+              # Text file - use bat
+              ${pkgs.bat}/bin/bat $argv
+            else
+              # Binary file or unable to detect - use real cat
+              command cat $argv
+            end
+          else
+            # Not a file (stdin, directory, etc) - use bat
+            ${pkgs.bat}/bin/bat $argv
+          end
+        '';
+        shellAliases."cat!" = "command cat";
+      };
     };
   };
 }
